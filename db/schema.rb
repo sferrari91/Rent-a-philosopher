1bca78ee6ca7aa73db5a2132c93dd78c80e55acb
Santiago Ferrari <113905797+sferrari91@users.noreply.github.com>
2023-04-14 19:06:22 +0200

    models were added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_165057) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_170558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "philosophers", force: :cascade do |t|
+    t.string "name"
+    t.string "quote"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "users_id", null: false
+    t.bigint "philosophers_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["philosophers_id"], name: "index_reservations_on_philosophers_id"
+    t.index ["users_id"], name: "index_reservations_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_165057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "philosophers", column: "philosophers_id"
+  add_foreign_key "reservations", "users", column: "users_id"
 end
